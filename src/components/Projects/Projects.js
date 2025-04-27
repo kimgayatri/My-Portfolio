ab578f6daaef414425a7fe283bed472e6b3973c0
kimgayatri <gayatrinehe6@gmail.com>
2025-04-27 17:52:58 +0530

    Update assets and components

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import API from "../../Assets/Projects/Restful API Development.jpeg";
+import Carbon from "../../Assets/Projects/Carbon footprint calculator.jpeg";
+import CRUD from "../../Assets/Projects/CRUD.png";
+import P5 from "../../Assets/Projects/P5.png";
+import AI from "../../Assets/Projects/AI Desktop Assistant.jpeg";
+import Codeditor from "../../Assets/Projects/Codeditor.png";
 
 function Projects() {
   return (
@@ -23,7 +23,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={P5}
               isBlog={false}
               title="Project Management Tool"
               description="Project Management Tool with React and ExpressJS Framework (Frontend & Backend). The Project Managemnet tool with react and express is a comprehensive project management tool designed to streamline project planning, execution, and monitoring built using MERN Stack."
@@ -33,7 +33,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={Codeditor}
               isBlog={false}
               title="Real-Time-Code-Editor(Collaboration Tool)"
               description="The Real-Time Code Editor is an advanced web-based collaborative coding platform that allows multiple users to write, edit, and share code in real-time. Built using WebSockets, it ensures seamless synchronization, enabling developers to work together efficiently on the same codebase."
@@ -43,7 +43,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={CRUD}
               isBlog={false}
               title="MONGODB_CRUD_GUI"
               description="The MongoDB CRUD GUI is a web-based graphical user interface that simplifies Create, Read, Update, and Delete (CRUD) operations for MongoDB databases. It provides a user-friendly way to manage database records without requiring direct MongoDB shell or command-line interactions."
@@ -53,7 +53,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={API}
               isBlog={false}
               title="Restful-API-Development"
               description="The RESTful API Development project focuses on building a robust, scalable, and secure backend system using Node.js, Express.js, and MongoDB. This API enables seamless Create, Read, Update, and Delete (CRUD) operations, making it suitable for applications such as inventory systems, library management, user authentication services, and more."
@@ -63,24 +63,21 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={AI}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/kimgayatri"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="AI Desktop Assistant"
+              description="An AI desktop assistant is an intelligent software program that runs on a computer and helps users perform various tasks like opening applications, searching information, managing files, setting reminders, and answering questions using artificial intelligence. It can interact through text or voice commands, understand natural language, automate repetitive tasks, and sometimes learn user preferences over time to offer personalized support. AI desktop assistants make daily computer usage faster, easier, and more efficient by acting like a smart personal helper."
+              ghLink="https://github.com/kimgayatri/AI-Desktop-Assistant.git"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={Carbon}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/kimgayatri"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Carbon Footprint Calculator"
+              description="A carbon footprint calculator is a tool that estimates the amount of carbon dioxide (CO₂) and other greenhouse gases a person, organization, or activity produces based on their daily habits, such as travel, energy use, food consumption, and shopping. By answering a few questions, users can see how their lifestyle impacts the environment and get suggestions on how to reduce their emissions. It helps raise awareness and encourages more eco-friendly choices to fight climate change."
+              ghLink="https://github.com/kimgayatri/Carbon-Footprint-Calculator-App.git"
             />
           </Col>
         </Row>
